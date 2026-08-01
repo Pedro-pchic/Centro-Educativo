142f62316933dd02dcf0c2456996ca71fe38d4b1
gabriela127 <avillagranc3@miumg.edu.gt>
2026-07-31 23:23:44 -0600

    feat:creacion de la nueva rama 013-api-estudiante

--- sgau-backend-api/src/main/java/com/umg/sgau/exception/EstudianteNoEncontradoException.java
+++ sgau-backend-api/src/main/java/com/umg/sgau/exception/EstudianteNoEncontradoException.java
@@ -2,6 +2,8 @@ package com.umg.sgau.exception;
 
 public class EstudianteNoEncontradoException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public EstudianteNoEncontradoException(Long id) {
         super("Estudiante no encontrado con id: " + id);
     }
